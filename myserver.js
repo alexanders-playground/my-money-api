982c55ee65cdbacf3f88754bbd538db5847f120f
Alex Spence <mycodeinbox@gmail.com>
2019-02-17 08:34:18 +0000

    First commit from server

---
+++ myserver.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+app.get('/greeting', (req, res) => {
+  res.send('Welcome to the sample node js server !')
+})
+app.get('/who/:name', (req, res) => {
+  res.send('Hello '+ req.params.name)
+})
+app.get('/purchase', (req, res) => {
+  res.send('What would you like to buy?')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
