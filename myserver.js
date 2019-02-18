2d9ad0ad9fbb1286c14089b4fcc65fe713de21eb
Alex Spence <mycodeinbox@gmail.com>
2019-02-18 23:36:18 +1300

    Added functionality for postgresql in pg npm library. The APInow returns a budget object for consuming in the front end. CORS was added for development

--- myserver.js
+++ myserver.js
@@ -1,19 +1,116 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const {Pool, Client} = require('pg');
 
+const app = express()
+var cors = require('cors')
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionSuccessStatus: 200
+}
+
+const pool = new Pool({
+  user: "alexander",
+  host: "127.0.0.1",
+  database: "alexander",
+  port: "5432"
+})
+
+app.set('json spaces', 4);
+
+getPlaygroundItems = function(query){
+    return new Promise(function(resolve, reject){
+      pool.query(query, (err, res) => {
+        if(err){
+          throw err;
+        }else{
+          console.log(res.rows)
+          resolve(res.rows);
+          
+        }
+    })
+  })
+}
 
 //app.get('/', (req, res) => {
 //  res.send('HEY!')
 //})
 app.use(express.static('public'))
 
-app.get('/greeting', (req, res) => {
-  res.send('Welcome to the sample node js server !')
+app.get('/budget', cors(corsOptions), (req, res) => {
+  //Get the information out of the database and send it to the front end
+  console.log(req)
+    incomeStreams = [
+      {
+        "key": 1,
+        "name": "Paycheck",
+        "amount": 2000,
+        "frequency": 2,
+      },
+      {
+        "key": 2,
+        "name": "Consulting",
+        "amount": 500,
+        "frequency": 1,
+      },
+      {
+        "key": 3,
+        "name": "Investments",
+        "amount": 1000,
+        "frequency": 1,
+      }
+    ]
+    expenses = [
+      {
+        "key": 1,
+        "name": "Mortgage",
+        "amount": 1300,
+        
+      },
+      {
+        "key": 2,
+        "name": "Internet",
+        "amount": 60,
+        
+      },
+      {
+        "key": 3,
+        "name": "Phone",
+        "amount": 60,
+        
+      },
+      {
+        "key": 4,
+        "name": "Liquor",
+        "amount": 200,
+        
+      },
+      {
+        "key": 5,
+        "name": "Petrol",
+        "amount": 200, 
+      }
+    ]
+  
+  res.send({incomeStreams, expenses})
 })
+
+
 app.get('/who/:name', (req, res) => {
   res.send('Hello '+ req.params.name)
 })
-app.get('/purchase', (req, res) => {
-  res.send('What would you like to buy?')
+
+
+app.get('/playground', (req, res) => {
+  var query = 'SELECT * from playground;'
+
+  getPlaygroundItems(query).then(function(results){
+    res.send({data: results})
+  }).catch(function(err){
+    console.log(err)
+    res.send("Didn't find anything");
+  })
+  
 })
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+app.listen(5000, () => console.log('Server running on port 5000'))
