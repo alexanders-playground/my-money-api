934ff3fe6d23bade85606c7e1a6e7977a3aa215a
Alex Spence <mycodeinbox@gmail.com>
2019-03-13 19:38:12 +1300

    CORS removal

--- myserver.js
+++ myserver.js
@@ -13,84 +13,21 @@ app.set('json spaces', 4);
 const { Budget, IncomeStream, Expense } = require('./db');
 
 
-var corsOptions = {
+/*var corsOptions = {
   origin: "http://localhost:3000",
   optionSuccessStatus: 200
-}
+}*/
 
 var BudgetController = require('./Controllers/Budget');
 
 
-
-
-//app.get('/', (req, res) => {
-//  res.send('HEY!')
-//})
-
 app.use(express.static('public'));
-app.use('/budget', cors(), BudgetController);
+app.use('/budget', BudgetController);
 
 app.get('/who/:name', (req, res) => {
   res.send('Hello '+ req.params.name)
 })
 
-app.get('/test', cors(), (req, res) => {
-  //Get the information out of the database and send it to the front end
-  console.log(req)
-    incomeStreams = [
-      {
-        "key": 1,
-        "name": "Paycheck",
-        "amount": 2000,
-        "frequency": 2,
-      },
-      {
-        "key": 2,
-        "name": "Consulting",
-        "amount": 500,
-        "frequency": 1,
-      },
-      {
-        "key": 3,
-        "name": "Investments",
-        "amount": 1000,
-        "frequency": 1,
-      }
-    ]
-    expenses = [
-      {
-        "key": 1,
-        "name": "Mortgage",
-        "amount": 1300,
-
-      },
-      {
-        "key": 2,
-        "name": "Internet",
-        "amount": 60,
-
-      },
-      {
-        "key": 3,
-        "name": "Phone",
-        "amount": 60,
-
-      },
-      {
-        "key": 4,
-        "name": "Liquor",
-        "amount": 200,
-
-      },
-      {
-        "key": 5,
-        "name": "Petrol",
-        "amount": 200,
-      }
-    ]
-
-  res.send({incomeStreams, expenses})
-})
 
 
 var server = app.listen(port, function(){
